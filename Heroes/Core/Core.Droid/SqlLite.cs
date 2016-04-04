79a285928fb9a0f2179789e4b57984d236b57aac
Michele Scandura <michele.scandura@outlook.com>
2016-04-04 16:22:49 +0100

    fixing db creation

--- Heroes/Core/Core.Droid/SqlLite.cs
+++ Heroes/Core/Core.Droid/SqlLite.cs
@@ -4,21 +4,22 @@ using Core.Services;
 using Xamarin.Forms;
 using SQLite.Net.Platform.XamarinAndroid;
 
-[assembly: Dependency (typeof(SqLiteAndroid))]
+[assembly: Dependency(typeof(SqLiteAndroid))]
 namespace Core.Droid
 {
-	public class SqLiteAndroid : ISqLite
-	{
-		private const string SqliteFilename = "Heroes.db3";
+    public class SqLiteAndroid : ISqLite
+    {
+        private const string SqliteFilename = "Heroes.db3";
 
-		public SQLite.Net.SQLiteConnection GetConnection ()
-		{
-			var documentsPath = System.Environment.GetFolderPath (System.Environment.SpecialFolder.Personal); // Documents folder
-			var path = Path.Combine (documentsPath, SqliteFilename);
-			// Create the connection
-			var conn = new SQLite.Net.SQLiteConnection (new SQLitePlatformAndroid (), SqliteFilename);
-			// Return the database connection
-			return conn;
-		}
-	}
+        public SQLite.Net.SQLiteConnection GetConnection()
+        {
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal); // Documents folder
+            var path = Path.Combine(documentsPath, SqliteFilename);
+            // Create the connection
+
+            var conn = new SQLite.Net.SQLiteConnection(new SQLitePlatformAndroid(), path);
+            // Return the database connection
+            return conn;
+        }
+    }
 }
